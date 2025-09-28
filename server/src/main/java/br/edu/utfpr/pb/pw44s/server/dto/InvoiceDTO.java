004dd6b22a5c3e17a0a03a1e3eec4eb3c4e35169
Valber Sales <valbersalesjunior@gmail.com>
2025-09-28 00:58:48 -0300

    Fix: Ajuste no InvoiceDTO para exibir corretamente os dados de endereço e pagamento

--- server/src/main/java/br/edu/utfpr/pb/pw44s/server/dto/InvoiceDTO.java
+++ server/src/main/java/br/edu/utfpr/pb/pw44s/server/dto/InvoiceDTO.java
@@ -1,5 +1,7 @@
 package br.edu.utfpr.pb.pw44s.server.dto;
 
+import br.edu.utfpr.pb.pw44s.server.model.InvoiceAddressEmbeddable;
+import br.edu.utfpr.pb.pw44s.server.model.InvoicePaymentEmbeddable;
 import br.edu.utfpr.pb.pw44s.server.model.InvoiceStatus;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,9 +17,10 @@ public class InvoiceDTO {
     private Long id;
     private LocalDate date;
     private InvoiceStatus status;
+    private InvoicePaymentEmbeddable payment;
     private BigDecimal total;
     private String trackingCode;
     private UserDTO user;
-    private AddressDTO address;
+    private InvoiceAddressEmbeddable address;
     private List<InvoiceItemsDTO> items;
 }
