f219f36ba865a5ff9f5a9e56ad6355b2875a76e5
valber <valbersalesjunior@gmail.com>
2025-11-16 18:35:30 -0300

    feat: implementação inicial do front end

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { AppRoutes } from './routes/app-routes'
+
+function App() {
+  // react-developer-tools
+  return (
+    <>
+      <AppRoutes />
+    </>
+  )
+}
+
+export default App
